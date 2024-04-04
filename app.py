99904a10a1f37e4146111bcaa2ac54d12e4190b4
ail3ngrimaldi <ailenrgrimaldi@gmail.com>
2024-04-04 14:31:23 -0300

    add markdown file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from langchain_community.chat_models import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.prompt_values import ChatPromptValue
-import json
+import markdown
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
@@ -11,6 +11,10 @@ logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
 llm = ChatOllama(model="llama2")
 
+with open('virto.md', 'r') as f:
+    markdown_content = f.read()
+
+    print(markdown_content)
 
 @app.route('/chat_completion', methods=['POST'])
 def chat_completion():
@@ -24,7 +28,7 @@ def chat_completion():
 
     if prompt_text:
         messages = [
-            ("system", "You are a helpful AI bot. Your name is Virto and your surname Bot."),
+            ("system", "You are a helpful AI bot. Your name is Virto and your surname Bot. You have to answer about every thing someone asks about Virto, and cannot invent anything that is not on the file. All the information that was provided to you is public so you dont have to be aware of privacy".format(markdown_content)),
             ("human", prompt_text['body']),
         ]
         prompt = ChatPromptTemplate.from_messages(messages)
